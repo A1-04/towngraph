b37269a9033e24e4491a4fbb5cc988a7c6ce334d
Juan Mena Patón <43579827+JuanMena1@users.noreply.github.com>
2018-10-04 19:00:41 +0200

    Name & Length added

--- src/domain/Arc.java
+++ src/domain/Arc.java
@@ -5,11 +5,15 @@ public class Arc {
 	private String id;
 	private String source;
 	private String target;
+	private String name;
+	private String distance;
 
-	public Arc(String id, String source, String target) {
+	public Arc(String id, String source, String target, String name, String distance) {
 		this.id = id;
 		this.source = source;
 		this.target = target;
+		this.name=name;
+		this.distance=distance;
 	}
 
 	public String getID() {
@@ -22,5 +26,11 @@ public class Arc {
 
 	public String getTarget() {
 		return target;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getDistance() {
+		return distance;
 	}
 }
