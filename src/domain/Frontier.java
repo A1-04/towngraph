d37ab7ce550ccaa9b4c28959b246630e99561192
sergio.herrera2 <sergio.herrera2@alu.uclm.es>
2018-10-16 12:34:37 +0200

    Second part of the practise started

---
+++ src/domain/Frontier.java
@@ -0,0 +1,39 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class Frontier {
+	private ArrayList<TreeNode> treenodes;
+
+	public Frontier() {
+
+	}
+
+	public ArrayList<TreeNode> createFrontier() {
+		treenodes = new ArrayList<TreeNode>();
+		Collections.sort(treenodes, new Comparator<TreeNode>() {
+			public int compare(TreeNode a, TreeNode b) {
+				return new Integer(a.getF()).compareTo(new Integer(b.getF()));
+			}
+		});
+		return treenodes;
+	}
+
+	public void insert(TreeNode tn) {
+		treenodes.add(tn);
+	}
+
+	public TreeNode remove() {
+		return treenodes.remove(0);
+	}
+
+	public boolean isEmpty() {
+		if (treenodes.size() == 0) {
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ src/domain/TreeNode.java
@@ -0,0 +1,65 @@
+package domain;
+
+import java.util.ArrayList;
+
+public class TreeNode {
+	private Node parent;
+	private State currentState;
+	private int pathcost;
+	private ArrayList<State> action;
+	private int d;
+	private float f; // value that determines the insertion order in the frontier
+
+	public TreeNode() {
+		this.f = (float) ((Math.random() * 1000) + 1);
+	}
+
+	public Node getParent() {
+		return parent;
+	}
+
+	public void setParent(Node parent) {
+		this.parent = parent;
+	}
+
+	public State getCurrentState() {
+		return currentState;
+	}
+
+	public void setCurrentState(State currentState) {
+		this.currentState = currentState;
+	}
+
+	public int getPathcost() {
+		return pathcost;
+	}
+
+	public void setPathcost(int pathcost) {
+		this.pathcost = pathcost;
+	}
+
+	public ArrayList<State> getAction() {
+		return action;
+	}
+
+	public void setAction(ArrayList<State> action) {
+		this.action = action;
+	}
+
+	public int getD() {
+		return d;
+	}
+
+	public void setD(int d) {
+		this.d = d;
+	}
+
+	public int getF() {
+		return f;
+	}
+
+	public void setF(int f) {
+		this.f = f;
+	}
+
+}
