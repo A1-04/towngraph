76cfa26248122dd0b6c3c4a47141bf45e2f4669d
Juan Mena Patón <Juan.Mena1@alu.uclm.es>
2018-11-05 19:23:57 +0100

    P3 ready and Algorithm ready for implement

---
+++ src/domain/TSFAlgorithm.java
@@ -0,0 +1,48 @@
+package domain;
+
+import java.util.Hashtable;
+
+public class TSFAlgorithm {
+	private Hashtable<String, Float> VL = new Hashtable<String, Float>();
+	
+	
+	public TSFAlgorithm() {
+		
+	}
+
+	public TreeNode BFS() {
+
+		return null;
+	}
+
+	public TreeNode DFS(int depth) {
+		return null;
+	}
+
+	public TreeNode UCS() {
+		return null;
+	}
+	
+	public TreeNode IDS(int depth) {
+		return null;
+	}
+	
+	public TreeNode prBFS() {
+
+		return null;
+	}
+
+	public TreeNode prDFS(int depth) {
+		return null;
+	}
+
+	public TreeNode prUCS() {
+		return null;
+	}
+	
+	public TreeNode prIDS(int depth) {
+		return null;
+	}
+	
+	
+}
--- src/presentation/P3.java
+++ src/presentation/P3.java
@@ -2,16 +2,52 @@ package presentation;
 
 import java.util.Hashtable;
 
+import domain.TSFAlgorithm;
+import domain.TreeNode;
+
+
 public class P3 {
 	public static void  main(String[]args) {
 		String Jname = "";
 		int depth = 0;
-		String Technique = "";
+		String technique = "";
 		boolean prunning = false;
-		
-		Hashtable<String, Float> VL = new Hashtable<String, Float>();
-		
-		
+
+		TSFAlgorithm alg = new TSFAlgorithm();
+		TreeNode sol = new TreeNode();
+
+		switch(technique) {
+		case "BFS":
+			if(!prunning) {
+				sol = alg.BFS();
+			}else {
+				sol = alg.prBFS();
+			}
+			break;
+		case "DFS":
+			if(!prunning) {
+				sol = alg.DFS(depth);
+			}else {
+				sol = alg.prDFS(depth);
+			}
+			break;
+		case "UCS":
+			if(!prunning) {
+				sol = alg.UCS();
+			}else {
+				sol = alg.prUCS();
+			}
+			break;
+		case "IDS":
+			if(!prunning) {
+				sol = alg.IDS(depth);
+			}else {
+				sol = alg.prIDS(depth);
+			}
+			break;
+		default:
+			System.err.println("This is not a valid option");
+		}
 	}
 
 }
