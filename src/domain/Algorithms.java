1a8b328dee6ebdc20b528936b5bb0a69c9309afc
Juan Mena Patón <Juan.Mena1@alu.uclm.es>
2018-11-05 19:07:35 +0100

    P3 and Algorithms created

---
+++ src/domain/Algorithms.java
@@ -0,0 +1,17 @@
+package domain;
+
+public class Algorithms {
+
+	public TreeNode BFS(boolean prunning) {
+
+		return null;
+	}
+
+	public TreeNode DFS(boolean prunning, int depth) {
+		return null;
+	}
+
+	public TreeNode UCS(boolean prunning) {
+		return null;
+	}
+}
---
+++ src/presentation/P3.java
@@ -0,0 +1,17 @@
+package presentation;
+
+import java.util.Hashtable;
+
+public class P3 {
+	public static void  main(String[]args) {
+		String Jname = "";
+		int depth = 0;
+		String Technique = "";
+		boolean prunning = false;
+		
+		Hashtable<String, Float> VL = new Hashtable<String, Float>();
+		
+		
+	}
+
+}
